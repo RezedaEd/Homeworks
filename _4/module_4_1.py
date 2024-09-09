e06692e54fe6f09d8db186a53878b13d6afcf09d
Резеда <rezeda.khazieva.88@gmai.ru>
2024-09-09 16:53:14 +0300

    module_4_1

---
+++ _4/module_4_1.py
@@ -0,0 +1,8 @@
+from _4.face_math import divide as div_f
+from true_math import divide as div_t
+
+res1 = div_f(50,0)
+res2 = div_f(11,3)
+res3 = div_t(88, 4)
+res4 = div_t(7,0)
+print(res1, res2, res3, res4, sep='\n')
