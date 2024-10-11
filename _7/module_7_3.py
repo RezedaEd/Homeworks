c8a7db87129c563331ffde8fe3a358b0a3ed9abc
Резеда <rezeda.khazieva.88@gmai.ru>
2024-10-11 17:41:08 +0300

    module_7_3_

--- _7/module_7_3.py
+++ _7/module_7_3.py
@@ -1,48 +1,43 @@
+import string
 class WordsFinder:
-    def __init__(self, *files):
-        file_names = []
-        self.file_names = file_names
-        for file_name in files:
-            self.file_names.append(file_name)
+    def __init__(self, *files_names):
+        self.files_names = files_names
+
 
     def get_all_words(self):
         all_words = {}
-        for name in self.file_names:
+        for name in self.files_names:
             with open(name, encoding='utf-8') as file:
                 p = [',', '.', '=', '!', '?', ';', ':', ' - ']
                 words = []
                 for line in file:
-                    for i in p:
-                        line = line.replace(i, '')
+                    line = line.translate(str.maketrans('', '', string.punctuation.replace('-', '')))
                     words += line.lower().split()
                 all_words[name] = words
         return all_words
 
     def find(self, word):
         word = word.casefold()
-        find_dict = {}
+        result_dict = {}
         for name, words in self.get_all_words().items():
             if word in words:
-                find_dict[name] = words.index(word) + 1
-                return find_dict
+                result_dict[name] = words.index(word) + 1
             else:
                 return 'Искомое слово не найдено.'
+        return result_dict
 
     def count(self, word):
         word = word.casefold()
-        count_dict = {}
+        result_dict = {}
         count = 0
         for name, words in self.get_all_words().items():
-            for i in words:
-                if i == word:
-                    count += 1
-                count_dict[name] = count
-        return count_dict
+            result_dict[name] = words.count(word)
+        return result_dict
 
 
 
-d = WordsFinder('Rudyard Kipling - If.txt')
-print(d.file_names)
+d = WordsFinder('Rudyard Kipling - If.txt', 'Walt Whitman - O Captain! My Captain!.txt')
+print(d.files_names)
 print(d.get_all_words())
 print(d.find('tHe'))
 print(d.count('tHE'))
